b681df3d85ed2a599a73dd9155846bb6776e5436
unknown <recarvalhoe@gmail.com>
2025-06-10 16:26:37 -0300

    Exercicio 1-8 Finalizado

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 900,
   viewportWidth: 1280,
-  e2e: {},
+  e2e: {
+  },
+  video: true,
+  videosFolder: 'cypress/videos',
+  setupNodeEvents(on, config) {
+    console.log("Configuração do Cypress:", config); 
+  },
+  // pageLoadTimeout: 120000, // Increase to 120 seconds (2 minutes)
+  // defaultCommandTimeout: 10000, // Optional: Increase default command timeout as well
 })
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -151,4 +151,17 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       })
   })
 
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  })
+
 })
