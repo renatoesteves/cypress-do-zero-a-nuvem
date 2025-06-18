14e558abd570b25b2d55ca96f9cd02d86188ae8b
unknown <recarvalhoe@gmail.com>
2025-06-18 11:58:17 -0300

    Removing set only

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -200,7 +200,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('have.value', 'Um texto qualquer')
   })
 
-  it.only('faz uma requisição HTTP', () => {
+  it('faz uma requisição HTTP', () => {
     cy.request('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
       .as('getRequest')
       .its('status')
